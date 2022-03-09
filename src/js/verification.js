c8a9964cb74e747ec789d6408418f6f42182418b
ale <ale@ale.com>
2022-03-09 10:59:22 -0300

    fix: fixing play of player, create class contructor of player

--- src/js/verification.js
+++ src/js/verification.js
@@ -1,6 +1,6 @@
 import { endGame, showAgain, showSequenceColors } from "./base.js";
 import { machine } from "./test_machine.js";
-import { playerArray } from "./test_player.js";
+import { player } from "./test_player.js";
 
 const buttonStart = document.getElementById('btn-send')
 
@@ -17,20 +17,14 @@ buttonStart.addEventListener('click', function () {
 
 
 function verification() {
-    
-    
-    if (machine.random.length === playerArray.length) {
+    if (machine.random.length === player.playerArray.length) {
 
         for (let counter = 0; counter < machine.random.length; counter++) {
-            if (machine.random[counter].toLowerCase() !== playerArray[counter].toLowerCase()) {
-                console.log(machine.random[counter])
-                console.log(playerArray[counter])
+            if (machine.random[counter].toLowerCase() !== player.playerArray[counter].toLowerCase()) {
                 endGame()
             }
         }
-        
-       
-       
+
         const box_blue = document.getElementById('blue');
         const box_red = document.getElementById('red');
         const box_yellow = document.getElementById('yellow');
@@ -40,11 +34,10 @@ function verification() {
         box_red.classList.remove('animationred');
         box_yellow.classList.remove('animationyellow');
         box_green.classList.remove('animationgreen');
-        
-        showAgain()
-        console.log('array machine' + machine.random)
-    }
 
+        player.playerArray = []
+        showAgain()
+    }
 }
 
 export { verification }
