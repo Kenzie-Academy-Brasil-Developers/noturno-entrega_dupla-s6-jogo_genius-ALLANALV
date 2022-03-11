e966d65ff0e13d5df4c76cd46e4b511f60e0b283
thiago <thiagolbf@hotmail.com>
2022-03-11 20:59:40 -0300

    addClickBright

--- src/js/clickplayer.js
+++ src/js/clickplayer.js
@@ -16,24 +16,52 @@ const yellowDiv = document.getElementById('yellow')
 
 blueDiv.addEventListener('click', function () {
     player.playerArray.push('blue')
+
+    blueDiv.classList.replace('darkBlue', 'ligthBlue')
+    
+    setTimeout(() => {
+        blueDiv.classList.replace('ligthBlue', 'darkBlue')
+    }, 400)
+
     verification()
 
 })
 
 redDiv.addEventListener('click', function () {
     player.playerArray.push('red')
+    
+    redDiv.classList.replace('darkRed', 'ligthRed')
+    
+    setTimeout(() => {
+        redDiv.classList.replace('ligthRed', 'darkRed')
+    }, 400)
+
     verification()
 
 })
 
 greenDiv.addEventListener('click', function () {
     player.playerArray.push('green')
+
+    greenDiv.classList.replace('darkGreen', 'ligthGreen')
+    
+    setTimeout(() => {
+        greenDiv.classList.replace('ligthGreen', 'darkGreen')
+    }, 400)
+    
     verification()
 
 })
 
 yellowDiv.addEventListener('click', function () {
     player.playerArray.push('yellow')
+
+    yellowDiv.classList.replace('darkYellow', 'ligthYellow')
+
+    setTimeout(() => {
+        yellowDiv.classList.replace('ligthYellow', 'darkYellow')
+    }, 400)
+
     verification()
 
 })
